1e37f6e62e6ad44ae0a946cdede4b20f995e0926
Sky Bai(白喜阳) <baixiyang@inspur.com>
2019-04-13 00:27:55 +0800

    echarts for date

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -1,5 +1,58 @@
 // pages/detail/detail.js
-const app = getApp()
+import * as echarts from '../../packages/ec-canvas/echarts';
+
+const app = getApp();
+function initChart(canvas, width, height) {
+    const chart = echarts.init(canvas, null, {
+        width: width,
+        height: height
+    });
+    canvas.setChart(chart);
+
+    var option = {
+        backgroundColor: "#ffffff",
+        color: ["#37A2DA", "#32C5E9", "#67E0E3", "#91F2DE", "#FFDB5C", "#FF9F7F"],
+        series: [{
+            label: {
+                normal: {
+                    fontSize: 14
+                }
+            },
+            type: 'pie',
+            center: ['50%', '50%'],
+            radius: [0, '30%'],
+            data: [{
+                value: 55,
+                name: '北京'
+            }, {
+                value: 20,
+                name: '武汉'
+            }, {
+                value: 10,
+                name: '杭州'
+            }, {
+                value: 20,
+                name: '广州'
+            }, {
+                value: 38,
+                name: '上海'
+            },
+            ],
+            itemStyle: {
+                emphasis: {
+                    shadowBlur: 10,
+                    shadowOffsetX: 0,
+                    shadowColor: 'rgba(0, 2, 2, 0.3)'
+                }
+            }
+        }]
+    };
+
+    chart.setOption(option);
+    return chart;
+}
+
+
 Page({
 
   /**
@@ -8,9 +61,14 @@ Page({
   data: {
       signature:'',
       comment:{name:'科比',content:'打篮球我谁都不服'},
-      dateRecord:{appoint:'9',stand:'1'}
+      dateRecord:{appoint:'9',stand:'1'},
+      ec: {
+      }
 
   },
+    echartInit(e) {
+        initChart(e.detail.canvas, 100, 100);
+    },
 
   /**
    * 生命周期函数--监听页面加载
@@ -27,6 +85,7 @@ Page({
   onReady: function () {
 
   },
+
 
   /**
    * 生命周期函数--监听页面显示
